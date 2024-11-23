b3f557b0e7c985dd2f72fa248a72cadb21346fd6
AAROHI128 <151994430+AAROHI128@users.noreply.github.com>
2024-11-23 15:25:32 +0530

    Update schema.ts

--- utils/schema.ts
+++ utils/schema.ts
@@ -16,16 +16,15 @@ export const students = pgTable('students', {
   email: varchar('email', { length: 255 }).notNull().unique(),
   batchId: integer('batch_id').references(() => batch.id), // Foreign key to the `batch` table
   createdAt: date('created_at').defaultNow(),
-  createdBy: integer('created_by').notNull(),
-  
+  createdBy: integer('created_by').notNull(), 
 });
 
 
 // Define the `attendance` table
 export const attendance = pgTable('attendance', {
   id: serial('id').primaryKey(),
-  studentId: integer('student_id').notNull().references(() => students.id), // Foreign key to the `students` table
+  studentId: integer('student_id').notNull(), // Foreign key to the `students` table
   date: date('date').notNull(),
-  isPresent: boolean('is_present').notNull(),
+  isPresent: boolean('is_present').default(false),
   createdAt: date('created_at').defaultNow(),
 });
